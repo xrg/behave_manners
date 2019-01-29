a800ba05a73eb1ba207c5ccbc0d223ca41013126
P. Christeas <xrg@linux.gr>
2019-01-29 02:24:11 +0200

    helpers: implement a mutable Integer class
    Integer can be passed by reference, modified in-place

--- behave_manners/pagelems/helpers.py
+++ behave_manners/pagelems/helpers.py
@@ -1,5 +1,6 @@
 # -*-coding: UTF-8 -*-
 
+from __future__ import division
 import re
 
 
@@ -32,5 +33,127 @@ def prepend_xpath(pre, xpath):
         return pre + xpath[2:]
 
     return pre + xpath
+
+
+class Integer(object):
+    """Mutable integer implementation
+    
+        Useful for counters, that need to be passed by reference
+    """
+
+    __slots__ = ('_value',)
+
+    def __init__(self, value):
+        if not isinstance(value, (int, Integer)):
+            raise TypeError("Cannot convert from %s to integer" % type(value))
+        self._value = value
+
+    def __int__(self):
+        return self._value
+
+    def __repr__(self):
+        return str(self._value)
+
+    def __bool__(self):
+        return self._value
+
+    __nonzero__ = __bool__
+
+    def __lt__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+        return self._value < other
+
+    def __le__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+        return self._value <= other
+
+    def __gt__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+        return self._value > other
+
+    def __ge__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+        return self._value >= other
+
+    def __eq__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return self._value == other
+
+    def __abs__(self):
+        return Integer(abs(self._value))
+
+    def __add__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return Integer(self._value + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return Integer(self._value - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return Integer(self._value * other)
+
+    def __mod__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return Integer(self._value % other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return self._value / other
+
+    def __floordiv__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        return self._value // other
+
+    def __iadd__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        self._value += other
+        return self
+
+    def __isub__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        self._value -= other
+        return self
+
+    def __itruediv__(self, other):
+        raise RuntimeError("Integer only supports floor division inplace")
+
+    def __ifloordiv__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        self._value //= other
+        return self
+
+    def __imul__(self, other):
+        if isinstance(other, Integer):
+            other = other._value
+
+        self._value *= other
+        return self
+
 
 # eof
