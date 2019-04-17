aa8bf17af78dcb0400d299a6e7ecf1285ce90796
P. Christeas <xrg@linux.gr>
2019-04-17 09:33:37 +0300

    exceptions: decorate standard exceptions with component mixin
    Purpose is to still raise KeyError, AttributeError etc on object methods,
    but also have a way to reference the offending component in them.

--- behave_manners/pagelems/exceptions.py
+++ behave_manners/pagelems/exceptions.py
@@ -5,6 +5,9 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class ElementNotFound(NoSuchElementException):
+    """Raised when a `WebElement` cannot locate a child, refers to that parent
+
+    """
     def __init__(self, msg=None, screen=None, stacktrace=None, 
                  parent=None, selector=None, pagelem_name=None):
         if not msg:
@@ -37,5 +40,32 @@ class PageNotReady(AssertionError):
 class Timeout(AssertionError):
     pass
 
+
+class ComponentException(object):
+    """Mixin for exceptions that can refer to faulty component
+    
+        The component would be an instance of `ComponentProxy`
+    """
+    def __init__(self, component=None, msg=None):
+        self.component = component
+        self.msg = msg
+
+
+class CKeyError(KeyError, ComponentException):
+    def __init__(self, arg, msg=None, component=None):
+        KeyError.__init__(self, arg)
+        ComponentException.__init__(self, msg=msg, component=component)
+
+
+class CAttributeError(AttributeError, ComponentException):
+    def __init__(self, arg, msg=None, component=None):
+        AttributeError.__init__(self, arg)
+        ComponentException.__init__(self, msg=msg, component=component)
+
+
+class CAssertionError(AssertionError, ComponentException):
+    def __init__(self, arg, component=None):
+        AssertionError.__init__(self, arg)
+        ComponentException.__init__(self, component=component)
 
 #eof
