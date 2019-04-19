02c842d4985c7645a9a80e335a8ba7a6b9e497b8
P. Christeas <xrg@pefnos.com>
2019-04-19 11:39:45 +0300

    tests: fix unittests for scopes

--- tests/test_scopes.py
+++ tests/test_scopes.py
@@ -19,7 +19,8 @@ class TestPEScopes(object):
     def test_root(self):
         driver = DummyDriver()
         root_scope = DOMScope['.root']()
-        root_scope.isready_all(driver)
+        scope = DOMScope['wait.base'](root_scope)
+        scope.isready_all(driver)
         assert len(driver.called_scripts) == 1
         assert driver.called_scripts[0] == \
             "if (document.readyState != 'complete') { return 'document'; }\n" \
@@ -37,5 +38,5 @@ class TestPEScopes(object):
         assert driver.called_scripts[0] == \
             "if (document.readyState != 'complete') { return 'document'; }\n" \
             "if (window.jQuery && window.jQuery.active) { return 'jQuery'; }\n" \
-            "if(window.getAllAngularTestabilities().findIndex(x=>!x.isStable()) >= 0) {return 'angular';}", \
+            "if(window.getAllAngularTestabilities().findIndex(function(x) { return !x.isStable(); }) >= 0) {return 'angular';}", \
             driver.called_scripts[0]
