9a44f7dfba36746ab2457a771f97efd9d7cf08f5
P. Christeas <xrg@pefnos.com>
2019-10-08 02:07:40 +0300

    examples/material: slow mode, non-headless by cmdline options

--- examples/material/environment.py
+++ examples/material/environment.py
@@ -2,8 +2,16 @@
 
 import logging
 from behave_manners import site_setup
+import time
 
 
 def before_all(context):
-    site_setup(context, config='config.yaml')
+    extra_conf = {'browser': {} }
+    if context.config.userdata.get('no-headless', False):
+        extra_conf['browser']['headless'] = False
+    site_setup(context, config='config.yaml', extra_conf=extra_conf)
 
+
+def after_step(context, step):
+    if context.config.userdata.get('slow', False):
+        time.sleep(1.0)
